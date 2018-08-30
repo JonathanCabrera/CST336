054b4327094e063e7f74171db1e18b28fed694e1
Jonathan Cabrera <jocabrera@csumb.edu>
2018-08-30 17:39:01 +0000

    adding lab 1 files

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<html><body>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title> Jonathan's Web Portfolio</title>
+    </head>
+    <body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
